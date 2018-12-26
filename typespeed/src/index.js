89c23c67b8ff7fd88c735c2bb72cac2af3f1dfe3
JacobEkedahl <jacob.ekedahl@gmail.com>
2018-12-26 17:34:52 +0900

    init typespeed

---
+++ typespeed/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Game extends React.Component {
+    render() {
+        return(
+            <div>Hello</div>
+        )
+    }
+}
+
+ReactDOM.render(<Game />, document.getElementById('root'));
