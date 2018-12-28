b52cba2786d8fb7ad33fb582fd7b81189ce70e9c
JacobEkedahl <jacob.ekedahl@gmail.com>
2018-12-28 11:40:35 +0100

    basic game is completed, todo is to increase difficulty with time

--- typespeed/src/index.js
+++ typespeed/src/index.js
@@ -3,31 +3,218 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const words = ['polly', 'mom', 'cat', 'mouse', 'dog', 'lover', 'no', 'unthinkable']
+var currentTick = 0
+
+class Healthbar extends React.Component {
+    renderHealth = (i) => {
+        return (
+            <div
+                key={i}
+                className={this.props.canStart ? "Health" : "Hidden"}
+
+                style={{
+                    width: this.props.width,
+                }}>
+            </div>
+        )
+    }
+
+    createHealthbar = () => {
+        let h = []
+        for (let i = 0; i < this.props.lives; i++) {
+            h.push(this.renderHealth(i))
+        }
+
+        return h
+    }
+
+    render() {
+        return (
+            <div className={this.props.canStart ? "Healthbar" : "Hidden"}
+            >
+                {this.createHealthbar()}
+            </div>
+        )
+    }
+}
+class Words extends React.Component {
+    renderWord = (word, i) => {
+        return (
+            <div
+                className="Word"
+                key={i}
+                style={{
+                    left: this.props.x[i],
+                    top: this.props.y[i],
+                }}>
+                {word}
+            </div>
+        )
+    }
+
+    createWords = () => {
+        let w = []
+        for (let i = 0; i < this.props.words.length; i++) {
+            w.push(this.renderWord(this.props.words[i], i))
+        }
+
+        return w
+    }
+
+    render() {
+        return (
+            <div>
+                {this.createWords()}
+            </div>
+        )
+    }
+}
 class Game extends React.Component {
     constructor() {
         super();
         this.state = {
-            value: 'Test',
-            currentWords: {},
+            value: 'Begin typing',
+            allWords: [],
+            currentWords: [],
+            wordsX: [],
+            wordsY: [],
             points: 0,
+            generatewordInterval: 100,
+            speed: 50,
+            lives: 3,
+            currentInterval: 100,
+            widthHealth: 198,
+            canStart: false,
+            isStarted: false
         };
     }
 
     componentDidMount() {
-        let firstCurrent = {}
-        
-        for (let i = 0; i < words.length ; i++) {
-            firstCurrent[words[i]] = true
+        this.setState({
+            allWords: words,
+        })
+        this.init()
+    }
+
+    init = () => {
+        //cancel current thread, how?
+
+        this.setState({
+            value: 'Begin typing',
+            canStart: true,
+            isStarted: false,
+            currentWords: [],
+            wordsX: [],
+            wordsY: [],
+            points: 0,
+            generatewordInterval: 100,
+            speed: 50,
+            lives: 3,
+            currentInterval: 100,
+        })
+    }
+
+    generateWords = () => {
+        clearInterval(this.intervalId)
+        this.intervalId = setInterval(this.genWords, this.state.generatewordInterval);
+    }
+
+    start = () => {
+        this.setState({
+            isStarted: true
+        })
+
+        clearInterval(this.intervalId)
+        this.intervalId = setInterval(this.play, this.state.speed);
+    }
+
+    genWords = () => {
+        //get a random word
+        const randIndex = Math.floor(Math.random() * this.state.allWords.length);
+        const newWord = this.state.allWords[randIndex]
+        let newCurrent = this.state.currentWords.slice()
+        newCurrent.push(newWord)
+
+        //get random X position
+        const width = newWord.length * 20
+        const maxX = 600 - width
+        const randX = Math.floor(Math.random() * maxX)
+        let newX = this.state.wordsX.slice()
+        newX.push(randX);
+
+        //generate word outside of app
+        let newY = this.state.wordsY.slice();
+        newY.push(-50);
+
+        this.setState({
+            currentWords: newCurrent,
+            wordsX: newX,
+            wordsY: newY,
+        })
+    }
+
+    getIndexOfWordsOutside = () => {
+        let wordsOutside = []
+        let newY = this.state.wordsY.slice()
+        for (let i = 0; i < newY.length; i++) {
+            if (newY[i] + 1 > 550) {
+                wordsOutside.push(i)
+            }
+        }
+
+        return wordsOutside;
+    }
+
+    removeWordsOutsideBounds = (indexes) => {
+        indexes.forEach(element => {
+            this.removeEntry(element);
+        });
+
+        this.setState({
+            lives: this.state.lives - indexes.length,
+        })
+    }
+
+    move = () => {
+        let newY = this.state.wordsY.slice()
+        for (let i = 0; i < newY.length; i++) {
+            newY[i] += 1
         }
 
         this.setState({
-            currentWords: firstCurrent,
+            wordsY: newY,
         })
+    }
+
+    /*
+    this.setState({
+        currentInterval: getRandomInterval(this.state.generatewordInterval),
+    })*/
+    play = () => {
+        //generate words at a certain interval
+        currentTick += 1
+        if (currentTick === this.state.currentInterval) {
+            currentTick = 0
+            this.genWords()
+        }
+
+        const indexes = this.getIndexOfWordsOutside()
+        if (indexes.length > 0) {
+            this.removeWordsOutsideBounds(indexes);
+        }
+        //if lost then go back to start state
+        if (this.state.lives <= 0) {
+            clearInterval(this.intervalId);
+            this.init();
+            return;
+        }
+
+        this.move();
     }
 
     handleInput = (e) => {
         this.setState({
-            value: e.target.value,              
+            value: e.target.value,
         })
     }
 
@@ -38,23 +225,36 @@ class Game extends React.Component {
     }
 
     isMatch = (val) => {
-        if (this.state.currentWords[val] === true) {
+        if (this.state.currentWords.includes(val)) {
             return true
-        } 
+        }
         return false
+    }
+
+    removeEntry = (index) => {
+        const newCurrent = removeword(this.state.currentWords, index);
+        const newX = removeword(this.state.wordsX, index);
+        const newY = removeword(this.state.wordsY, index);
+
+        this.setState({
+            currentWords: newCurrent,
+            wordsX: newX,
+            wordsY: newY,
+        })
+    }
+
+    handleStart = () => {
+        if (this.state.canStart) {
+            this.start()
+        }
     }
 
     handleMatch = (val) => {
         //add point, remove word from current words
-
-        /** TODO: COPY MAP AND REMOVE ELEMENT THEN RESET CURRENTWORDS */
-        console.log('handle match')
-        let newCurrent = this.state.currentWords
-        newCurrent[val] =false
+        this.removeEntry(this.state.currentWords.indexOf(val))
 
         this.setState({
-            currentWords: newCurrent,
-            points: this.state.points +1,
+            points: this.state.points + 1,
         })
     }
 
@@ -64,36 +264,69 @@ class Game extends React.Component {
             if (this.isMatch(val)) {
                 this.handleMatch(val)
             }
-            
+
             this.clear();
         }
     }
 
+    handleFocused = () => {
+        this.clear();
+    }
+
     render() {
         return (
-
             <div
                 className="Game"
+                onClick={this.handleStart}
                 onKeyPress={this.handleKey}
                 style={{
                     width: 600,
                     height: 600,
                 }}>
 
+                <Healthbar
+                    lives={this.state.lives}
+                    width={this.state.widthHealth}
+                    canStart={this.state.isStarted}
+                ></Healthbar>
+
+                <Words
+                    x={this.state.wordsX}
+                    y={this.state.wordsY}
+                    words={this.state.currentWords}
+                >
+                </Words>
+
+                <div className={this.state.isStarted ? "Hidden" : "Intro"}>
+                    Click to start
+                </div>
+
                 <div className="WrapperWriteBar">
-                    <input 
+                    <input
                         className="WriteBar"
                         onChange={this.handleInput}
+                        onClick={this.handleFocused}
                         value={this.state.value}
-                    />  
+                    />
 
                     <div className="Score">
                         {this.state.points}
-                    </div>                      
+                    </div>
                 </div>
             </div>
         )
     }
+}
+
+function removeword(arr, indexOfWord) {
+    let newCurrent = arr.slice(0, indexOfWord)
+        .concat(arr.slice(indexOfWord + 1, arr.length))
+    return newCurrent
+}
+
+function getRandomInterval(current) {
+    let newInterval = Math.floor(Math.random() * current);
+    return newInterval
 }
 
 ReactDOM.render(<Game />, document.getElementById('root'));
